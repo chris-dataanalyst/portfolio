02ba25594fbd8dc449bff769acbd5326aba68606
Chris Lanata <chris.lanata@gmail.com>
2025-07-30 14:53:28 +0700

    Add files via upload

--- world layoffs/world layoffs.sql
+++ world layoffs/world layoffs.sql
@@ -1,7 +1,7 @@
 select *
 from layoffs;
 
--- the purpose is to make working table that separate from original table
+-- 1. We make a separate table so if we make any mistake, we have a back up
 
 create table layoffs_stg
 like layoffs;
@@ -10,17 +10,11 @@ insert layoffs_stg
 select *
 from layoffs;
 
--- looking for duplicate and deleting the duplicate data
+-- then we looking for duplicate and deleting the duplicate data
 
 select *, row_number () over(partition by company, location, industry, total_laid_off, 
-percentage_laid_off , 'date', stage, country, funds_raised_millions) as row_numb
+percentage_laid_off , 'date', stage, country, funds_raised_millions) as row_num
 from layoffs_stg;
-
-with duplicate_data as (select *, row_number () over(partition by company, location, industry, total_laid_off, 
-percentage_laid_off , 'date', stage, country, funds_raised_millions) as row_numb
-from layoffs_stg)
-delete from duplicate_data
-where row_numb > 1;
 
 CREATE TABLE `layoffs_stg2` (
   `company` text,
@@ -35,24 +29,34 @@ CREATE TABLE `layoffs_stg2` (
   `row_num` int
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- standardizing the data, industry data from various type like crypto, cryptocurrencirs to Crypto 
+insert into layoffs_stg2
+select *, row_number () over(partition by company, location, industry, total_laid_off, 
+percentage_laid_off , 'date', stage, country, funds_raised_millions) as row_num
+from layoffs_stg;
+
+delete 
+from layoffs_stg2
+where row_num>1;
+
+-- then we will standardize the data, we can check in the industry data 
+-- there are a lot of variances of crypto so we will standardize from various type like crypto, cryptocurrencirs to just Crypto 
+
+select distinct industry
+from layoffs_stg2;
 
 update layoffs_stg2
 set industry = "Crypto"
 where industry like "crypto%";
 
-insert into layoffs_stg2
-select *, row_number () over(partition by company, location, industry, total_laid_off, 
-percentage_laid_off , 'date', stage, country, funds_raised_millions) as row_numb
-from layoffs_stg;
-
--- continue to cleaning the data, trimming stage data that have some blanks
+-- we will continue to cleaning the data, trimming stage data that have some blanks
 
 update layoffs_stg2
 set stage = trim(stage);
 
--- filling up some industry data, based on company, if company name is same, but the other data is without industry, then we fill up from other data
+-- then we will fill up industry data, some row doesn't have industry data even though in the other row with same company name has
+-- so we will fill it based on company, if company name is same, but the other data is without industry, then we fill up from other data
 -- using self join to match the company name with the industry
+-- we will first set all the blanks data to null, so we can work with the data
 
 update layoffs_stg2
 set industry = null
@@ -72,7 +76,9 @@ set t1.industry = t2.industry
 where (t1.industry is null or t1.industry='')
 and t2.industry is not null;
 
--- lastly we drop the unused column, and unusable data that doesn't have any values in it
+-- And the last step of cleaning, we will drop any columns and row that we can't work with
+-- which in this case is the row_num column we use for duplicate and some rows that
+-- don't have both total_laid_off and percentage_laid_off data 
 
 alter table layoffs_stg2
 drop column row_num;
@@ -81,6 +87,8 @@ delete
 from layoffs_stg2
 where total_laid_off is null
 and percentage_laid_off is null;
+
+-- And now our data is ready!
 
 select *
 from layoffs_stg2;
